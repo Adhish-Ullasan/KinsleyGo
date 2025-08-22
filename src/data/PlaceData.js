a53406558968cbd8a19e5e583c87c0a7d7a4f661
Adhish-Ullasan <adhishullasan@gmail.com>
2025-08-22 11:41:04 +0530

    add placedata

---
+++ src/data/PlaceData.js
@@ -0,0 +1,26 @@
+export const placeData = [
+    {
+        placeimage : "/places/about-1.jpg"
+    },
+    {
+        placeimage : "/places/about-2.jpg"
+    },
+    {
+        placeimage : "/places/about-3.jpg"
+    },
+    {
+        placeimage : "/places/about-4.jpg"
+    },
+    {
+        placeimage : "/places/about-5.jpg"
+    },
+    {
+        placeimage : "/places/about-6.jpg"
+    },
+    {
+        placeimage : "/places/about-7.jpg"
+    },
+    {
+        placeimage : "/places/about-8.jpg"
+    },
+]
